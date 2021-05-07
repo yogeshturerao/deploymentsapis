7700dc5ddb9afbb23713601c159f4464c173cc16
yogesh <tureraoyogi4@gmail.com>
2021-05-07 08:24:50 +0530

    timezone.py

--- timezone/timezone.py
+++ timezone/timezone.py
@@ -3,9 +3,14 @@ import pytz
 import datetime
 
 def lambda_handler(event, context):
-
+    tempvar2=[]
     timezones = pytz.common_timezones
+    #timezones = pytz.all_timezones
+    for timezone in timezones:
+        tempvar = timezone[0:2]
+        if tempvar == 'US':
+            tempvar2.append(timezone)
     return {
         'statusCode': 200,
-        'body': {"TimeZones": timezones}
+        'body': {"TimeZones": tempvar2}
     }
